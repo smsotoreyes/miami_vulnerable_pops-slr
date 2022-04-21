84c0aa1b3cc01c5295e2124b1ecfde4d70b63d1e
smsotoreyes <98330025+smsotoreyes@users.noreply.github.com>
2022-04-21 14:03:34 -0400

    update

---
+++ slr_check.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 20 21:18:12 2022
+
+@author: sofia
+"""
+#%% modules
+
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+#%% 
+
+raw = gpd.read_file(
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
