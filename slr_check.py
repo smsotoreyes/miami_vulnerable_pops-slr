4d94cace246c006a0a6290aa3bd18b2bb2e4274f
smsotoreyes <98330025+smsotoreyes@users.noreply.github.com>
2022-04-22 16:23:20 -0400

    update

--- slr_check.py
+++ slr_check.py
@@ -10,10 +10,13 @@ Created on Wed Apr 20 21:18:12 2022
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
+import json 
 
 #%% 
 
-raw = gpd.read_file(
+raw = gpd.read_file("tl_2020_12086_roads.zip")
+
+
 
 
 
